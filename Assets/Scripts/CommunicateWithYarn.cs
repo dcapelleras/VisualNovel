3b2d6262460e057fbe8cde779838da0bfaf79744
hilenyx <hilencornejososa@gmail.com>
2023-03-01 18:54:47 +0100

    added transportation across scenaries and movement

--- Assets/Scripts/CommunicateWithYarn.cs
+++ Assets/Scripts/CommunicateWithYarn.cs
@@ -10,14 +10,22 @@ public class CommunicateWithYarn : MonoBehaviour
     DialogueRunner dialogueRunner;
     [SerializeField] GameObject charPrefab;
     private Dictionary<string, GameObject> characters = new Dictionary<string, GameObject>();
-    public List<Transform> locations = new List<Transform>();
+    //public List<Transform> locations = new List<Transform>();
+    //public List<Transform> camLocations = new List<Transform>();
+    public Vector3 camOffset;
+    public Vector3 playerOffset;
+    GameObject playerInstance;
+
+    Camera cam;
 
     private void Awake()
     {
+        cam = Camera.main;
         dialogueRunner = FindObjectOfType<DialogueRunner>();
         dialogueRunner.AddCommandHandler<int>("counter", GetNumber);
         dialogueRunner.AddCommandHandler<string, string>("spawnChar", MoveCharacter);
-        characters.Add("cube", charPrefab);
+        characters.Add("Player", charPrefab);
+        dialogueRunner.AddCommandHandler<string>("camera", MoveCamera);
     }
 
     private void Update()
@@ -27,9 +35,20 @@ public class CommunicateWithYarn : MonoBehaviour
             dialogueRunner.Dialogue.Stop(); //to change node in mid running you first stop it
             dialogueRunner.StartDialogue("SecondConversation"); //to start a new dialogue script
         }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            dialogueRunner.Dialogue.Stop(); //to change node in mid running you first stop it
+            dialogueRunner.StartDialogue("Start"); //to start a new dialogue script
+        }
     }
 
-    // Update is called once per frame
+    private void MoveCamera(string locationName) //in yarn script: <<camera Corridor>> !!HAVE TO CREATE DICTIONARY OF <string, Transform>
+    {
+        Vector3 location = GameObject.Find(locationName).transform.position;
+        location += camOffset;
+        cam.transform.position = location;
+    }
+
     void GetNumber(int i)
     {
         Debug.Log(i);
@@ -39,7 +58,12 @@ public class CommunicateWithYarn : MonoBehaviour
     {
         GameObject character = characters[characterName];
         Vector3 location = GameObject.Find(locationName).transform.position;
-        Instantiate(character).transform.position = location;
+        location += playerOffset;
+        if (playerInstance == null)
+        {
+            playerInstance = Instantiate(character);
+        }
+        playerInstance.transform.position = location;
     }
 
     [YarnCommand("something")] //HAS TO BE PUBLIC! HAS TO AFFECT AN OBJECT! MUST CALL IT USING NAME OF GAMEOBJECT
